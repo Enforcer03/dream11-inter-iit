8a9b55850ac423c6f37935468af5520245b38bb1
Gurmannat Sohal <iamgurmannatsohal@gmail.com>
2024-11-30 20:15:56 +0530

    merge conflicts

--- ui/app/components/pageTemplateNoTop.tsx
+++ ui/app/components/pageTemplateNoTop.tsx
@@ -1,13 +1,9 @@
 function PageTemplateWithoutTop({ children }: { children?: React.ReactNode }) {
   return (
     <div className="h-screen w-screen flex items-center justify-center outer-page">
-<<<<<<< Updated upstream
       <div className="w-[89rem] h-[59rem] main_window_without_top py-10 backdrop-blur-2xl flex flex-col items-center">
         <div className="w-full h-full flex flex-col items-center justify-center">{children}</div>
       </div>
-=======
-      <div className="w-[83rem] h-[55rem] main_window_without_top py-10 backdrop-blur-2xl flex flex-col items-center"></div>
->>>>>>> Stashed changes
     </div>
   );
 }
--- ui/app/components/playerInformation.tsx
+++ ui/app/components/playerInformation.tsx
@@ -14,9 +14,6 @@ interface ButtonComponentProps {
   child4?: React.ReactNode;
 }
 
-<<<<<<< Updated upstream
-function PlayerInformation({ title, child3, child4, selectedPlayer, hoveredPlayer }: ButtonComponentProps) {
-=======
 function PlayerInformation({ title, child2, child3, child4 }: ButtonComponentProps) {
   const formatName = (name: string) => {
     if (name.length > 13) {
@@ -31,125 +28,9 @@ function PlayerInformation({ title, child2, child3, child4 }: ButtonComponentPro
 
   const formattedChild2 = typeof child2 === 'string' ? formatName(child2) : child2;
 
->>>>>>> Stashed changes
   return (
     <div className="flex flex-col justify-center items-center uppercase w-[21rem] h-full font-quantico">
       <h1 className="uppercase text-dream11FontColor font-bold text-3xl mb-8 ">{title}</h1>
-<<<<<<< Updated upstream
-      <div className="rounded-full border-dream11FontColor border-2 profilePhoto w-44 h-44 mb-8 flex items-center justify-center">
-        <Image src={profilePhoto} alt="player" height={50} width={50} className="" />
-      </div>
-      <div className="flex align-center justify-center">
-        <table className="text-dream11FontColor text-left">
-          <tbody>
-            <tr>
-              <th>Team:</th>
-              <th>Nationality:</th>
-            </tr>
-            <tr>
-              <td>
-                <Image src={teamPhoto} alt="player" width={50} height={50} />
-              </td>
-
-              <td>
-                <Image src={flagPhoto} alt="player" width={50} height={50} />
-              </td>
-            </tr>
-            <tr>
-              <th>Name:</th>
-              <th>Position:</th>
-            </tr>
-            <tr>
-              <td>{selectedPlayer.name}</td>
-              <td>19</td>
-            </tr>
-            <tr>
-              <th>Overall Rating:</th>
-              <th>Bowling Rating:</th>
-            </tr>
-            <tr>
-              <td>
-                {selectedPlayer.fitnessRating}{" "}
-                <span>
-                  {hoveredPlayer ? (
-                    selectedPlayer.fitnessRating - hoveredPlayer.fitnessRating >= 0 ? (
-                      <span className="text-green-500">
-                        {"+ "}
-                        {selectedPlayer.fitnessRating - hoveredPlayer.fitnessRating}
-                      </span>
-                    ) : (
-                      <span className="text-red-500">
-                        {"- "}
-                        {hoveredPlayer.fitnessRating - selectedPlayer.fitnessRating}
-                      </span>
-                    )
-                  ) : null}
-                </span>
-              </td>
-              <td>
-                {selectedPlayer.bowlingRating}{" "}
-                <span>
-                  {hoveredPlayer ? (
-                    selectedPlayer.bowlingRating - hoveredPlayer.bowlingRating >= 0 ? (
-                      <span className="text-green-500">
-                        {"+ "}
-                        {selectedPlayer.bowlingRating - hoveredPlayer.bowlingRating}
-                      </span>
-                    ) : (
-                      <span className="text-red-500">
-                        {"- "}
-                        {hoveredPlayer.bowlingRating - selectedPlayer.bowlingRating}
-                      </span>
-                    )
-                  ) : null}
-                </span>
-              </td>
-            </tr>
-            <tr>
-              <th>Fielding Rating:</th>
-              <th>Batting Rating:</th>
-            </tr>
-            <tr>
-              <td>
-                {selectedPlayer.fieldingRating}{" "}
-                <span>
-                  {hoveredPlayer ? (
-                    selectedPlayer.fieldingRating - hoveredPlayer.fieldingRating >= 0 ? (
-                      <span className="text-green-500">
-                        {"+ "}
-                        {selectedPlayer.fieldingRating - hoveredPlayer.fieldingRating}
-                      </span>
-                    ) : (
-                      <span className="text-red-500">
-                        {"- "}
-                        {hoveredPlayer.fieldingRating - selectedPlayer.fieldingRating}
-                      </span>
-                    )
-                  ) : null}
-                </span>
-              </td>
-              <td>
-                {selectedPlayer.battingRating}{" "}
-                <span>
-                  {hoveredPlayer ? (
-                    selectedPlayer.battingRating - hoveredPlayer.battingRating >= 0 ? (
-                      <span className="text-green-500">
-                        {"+ "}
-                        {selectedPlayer.battingRating - hoveredPlayer.battingRating}
-                      </span>
-                    ) : (
-                      <span className="text-red-500">
-                        {"- "}
-                        {hoveredPlayer.battingRating - selectedPlayer.battingRating}
-                      </span>
-                    )
-                  ) : null}
-                </span>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-=======
       <div className="rounded-full border-2 profilePhoto w-44 h-44 mb-8 flex items-center justify-center">
         <Image src={child3} alt="player" height={50} width={50} className="" />
       </div>
@@ -195,14 +76,9 @@ function PlayerInformation({ title, child2, child3, child4 }: ButtonComponentPro
         </div>
           </div>
       </div>
->>>>>>> Stashed changes
       </div>
     </div>
   );
 }
-<<<<<<< Updated upstream
-export default PlayerInformation;
-=======
 
 export default PlayerInformation;
->>>>>>> Stashed changes
