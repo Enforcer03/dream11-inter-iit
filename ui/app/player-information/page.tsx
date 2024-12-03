e1e885c28e7e6318b5d005394a2b46e6633490de
Aryan <krish7664@gmail.com>
2024-12-03 21:39:00 +0530

    Adds scroll to player LLM text display

--- ui/app/player-information/page.tsx
+++ ui/app/player-information/page.tsx
@@ -121,7 +121,7 @@ export default function SwapPlayer() {
                 );
               })()}
           </div>
-          <div className="flex flex-col text-slate-300 w-[50rem] tracking-wider text-lg ml-[24rem] mb-[18rem] leading-relaxed overflow-hidden">
+          <div className="flex flex-col text-slate-300 w-[50rem] tracking-wider text-lg ml-[24rem] mb-[18rem] leading-relaxed overflow-y-scroll">
             {isLoading ? (
               <div className="loader-container">
                 <Image src="/loading.gif" alt="Loading..." width={100} height={100} priority />
